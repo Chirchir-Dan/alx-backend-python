4f00ac146de1fe93873b04ec57237eb7fa25acb5
Chirchir-Dan <1danchirchir@gmail.com>
2024-10-11 13:02:44 +0300

    Basic annotation function add that sums two float values and returns a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pyhton3
+"""
+This module represents a type annotated function add.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes arguments a and b, both of type float,
+    and returns their sum as a float.
+    """
+    return (a + b)
